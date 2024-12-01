0a395f487da61474154e17f81a59c1bd03d91ea2
Gleb Nazarov <gm.nazarov.work.@gmail.com>
2024-12-01 12:41:17 +0500

    Day01: post solutions

--- advent-of-code-2024/src/Day01.kt
+++ advent-of-code-2024/src/Day01.kt
@@ -1,21 +1,54 @@
+import kotlin.math.*
+
+private const val DAY = "Day01"
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val input = readInput(DAY)
+    val testInput = readInput("${DAY}_test")
+
+    partResults("Part 1", 11, part1(testInput)) { part1(input) }
+    partResults("Part 2", 31, part2(testInput)) { part2(input) }
+}
+
+private fun part1(places: List<String>): Long {
+    val teamsPlaces = splitTeamPlaces(places)
+
+    val firstTeamPlaces = teamsPlaces[0].sorted()
+    val secondTeamPlaces = teamsPlaces[1].sorted()
+
+    val distances = ArrayList<Long>()
+
+    for (i in firstTeamPlaces.indices) {
+        val distance = abs(firstTeamPlaces[i] - secondTeamPlaces[i])
+        distances.add(distance)
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    return distances.sum()
+}
+
+private fun part2(places: List<String>): Long {
+    val teamsPlaces = splitTeamPlaces(places)
+
+    val firstTeamPlaces = teamsPlaces[0]
+    val secondTeamPlaces = teamsPlaces[1].groupingBy { it }.eachCount()
+
+    var familiarity = 0L
+
+    firstTeamPlaces.forEach { place ->
+        val count = secondTeamPlaces.getOrDefault(place, 0)
+        familiarity += (place * count)
     }
+    return familiarity
+}
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+private fun splitTeamPlaces(places: List<String>): List<List<Long>> {
+    val firstTeamPlaces = ArrayList<Long>(places.size)
+    val secondTeamPlaces = ArrayList<Long>(places.size)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    for (place in places) {
+        val teamsPlaces = place.split("   ")
+        firstTeamPlaces.add(teamsPlaces[0].toLong())
+        secondTeamPlaces.add(teamsPlaces[1].toLong())
+    }
+    return listOf(firstTeamPlaces, secondTeamPlaces)
 }
--- advent-of-code-2024/src/Util.kt
+++ advent-of-code-2024/src/Util.kt
@@ -1,7 +1,7 @@
-import java.math.BigInteger
-import java.security.MessageDigest
-import kotlin.io.path.Path
-import kotlin.io.path.readText
+import java.math.*
+import java.security.*
+import kotlin.io.path.*
+import kotlin.time.*
 
 /**
  * Reads lines from the given input txt file.
@@ -15,7 +15,13 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
     .toString(16)
     .padStart(32, '0')
 
-/**
- * The cleaner shorthand for printing output.
- */
-fun Any?.println() = println(this)
+fun <T> partResults(part: String, expected: T, actual: T, calculation: () -> Any?) {
+    println("=== $part ===")
+    check(expected == actual)
+    println("Test: $actual")
+
+    val value: Any?
+    val time = measureTime { value = calculation() }
+    println("Answer: $value (done in $time)")
+    println()
+}
