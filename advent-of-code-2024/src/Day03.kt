071f1c60eebf4b2f25711b5943cb16b27b3dcc20
Gleb Nazarov <gm.nazarov.work@gmail.com>
2024-12-04 01:29:03 +0500

    Day03: part2 solution

--- advent-of-code-2024/src/Day03.kt
+++ advent-of-code-2024/src/Day03.kt
@@ -9,10 +9,11 @@ fun main() {
 
     partResults("Part 1", part1Expected, part1Answer) { part1(input) }
 
-//    val part2Expected = 0
-//    val part2Answer = part2(testInput)
+    val testInput2 = readInput("${DAY}_test2")
+    val part2Expected = 48
+    val part2Answer = part2(testInput2)
 
-//    partResults("Part 2", 0, part2(testInput)) { part2(input) }
+    partResults("Part 2", part2Expected, part2Answer) { part2(input) }
 }
 
 private val REGEX_PART1 = """mul\((\d{1,3}),(\d{1,3})\)""".toRegex()
@@ -21,6 +22,21 @@ private fun MatchResult.mul() = groupValues[1].toInt() * groupValues[2].toInt()
 
 private fun part1(input: String): Int = REGEX_PART1.findAll(input).sumOf { it.mul() }
 
-private fun part2(input: List<String>): Int = TODO()
+private val REGEX_PART2 = """don't\(\)|do\(\)|mul\((\d{1,3}),(\d{1,3})\)""".toRegex()
+
+private fun part2(input: String): Int {
+    var doIt = true
+    var sum = 0
+    REGEX_PART2.findAll(input).forEach { match ->
+        when (match.value) {
+            "do()" -> doIt = true
+            "don't()" -> doIt = false
+            else -> if (doIt) {
+                sum += match.mul()
+            }
+        }
+    }
+    return sum
+}
 
 private fun readInput(name: String) = readLines(name).joinToString("")
