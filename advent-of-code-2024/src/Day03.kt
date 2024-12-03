366526ca4e6a85bab671c40a8601e96d1deac4e1
Gleb Nazarov <gm.nazarov.work@gmail.com>
2024-12-04 01:16:41 +0500

    Day03: part1 refactor

--- advent-of-code-2024/src/Day03.kt
+++ advent-of-code-2024/src/Day03.kt
@@ -15,16 +15,12 @@ fun main() {
 //    partResults("Part 2", 0, part2(testInput)) { part2(input) }
 }
 
-private fun part1(input: List<List<Pair<Int, Int>>>): Int =
-    input.sumOf { line -> line.sumOf { it.pairMul() } }
+private val REGEX_PART1 = """mul\((\d{1,3}),(\d{1,3})\)""".toRegex()
+
+private fun MatchResult.mul() = groupValues[1].toInt() * groupValues[2].toInt()
+
+private fun part1(input: String): Int = REGEX_PART1.findAll(input).sumOf { it.mul() }
 
 private fun part2(input: List<String>): Int = TODO()
 
-private fun Pair<Int, Int>.pairMul() = this.first * this.second
-
-private fun readInput(name: String) = readLines(name) { str ->
-    val regex = "mul\\((\\d{1,3}),(\\d{1,3})\\)".toRegex()
-    regex.findAll(str)
-        .map { it.groupValues[1].toInt() to it.groupValues[2].toInt() }
-        .toList()
-}
+private fun readInput(name: String) = readLines(name).joinToString("")
