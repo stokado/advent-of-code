bff69ee3ce5cafdcfa586c0018e036719965d91e
Gleb Nazarov <gm.nazarov.work@gmail.com>
2024-12-07 21:51:01 +0500

    Day06: refactor,
    introduce guardWalk function to do different things on each step

--- advent-of-code-2024/src/Day06.kt
+++ advent-of-code-2024/src/Day06.kt
@@ -5,54 +5,88 @@ import libs.Direction.Companion.nextInDirection
 private const val DAY = "Day06"
 
 fun main() {
-    val input = readMatrix(DAY)
-    val testInput = readMatrix("${DAY}_test")
+    val input = readInput(DAY)
+    val testInput = readInput("${DAY}_test")
 
     val part1Expected = 41
     val part1Answer = part1(testInput)
 
     partResults("Part 1", part1Expected, part1Answer) { part1(input) }
 
-//    val part2Expected = 0
-//    val part2Answer = part2(testInput)
+    val part2Expected = 6
+    val part2Answer = part2(testInput)
 
-//    partResults("Part 2", part2Expected, part2Answer) { part2(input) }
+    partResults("Part 2", part2Expected, part2Answer) { part2(input) }
 }
 
-private fun part1(input: Matrix<Char>): Int {
-    val guard = Guard(input)
-    guard.walk()
+private fun part1(input: Input): Int {
+    val seenLocation = mutableSetOf<Position>()
+    guardWalk(input) { (position, _), _ -> seenLocation.add(position) }
 
-    var sum = 0
-    for (i in input.rowIndices) {
-        for (j in input.columnIndices) {
-            if (input[i, j] == 'X') {
-                sum++
+    return seenLocation.size
+}
+
+private fun part2(input: Input): Int {
+    var possibleNewObstacles = 0
+    return possibleNewObstacles
+}
+
+
+private fun guardWalk(
+    input: Input,
+    start: Position = input.start,
+    initialDirection: Direction = NORTH,
+    obstacles: Set<Position> = input.obstacles,
+    onEachStep: (State, State) -> Unit = { _, _ -> },
+) {
+    var position = start
+    var direction = initialDirection
+
+    while (position in input.bounds) {
+        val previousDirection = direction
+        var nextPosition = position.nextInDirection(direction)
+
+        while (nextPosition in obstacles) {
+            direction = direction.turnRight()
+            nextPosition = position.nextInDirection(direction)
+        }
+
+        onEachStep(position to previousDirection, nextPosition to direction)
+        position = nextPosition
+    }
+}
+
+private fun readInput(name: String): Input {
+    val lines = readLines(name)
+    val obstacles = mutableSetOf<Position>()
+    var start: Position? = null
+
+    for ((row, line) in lines.withIndex()) {
+        for ((column, char) in line.withIndex()) {
+            when (char) {
+                '#' -> obstacles += Position(row, column)
+                '^' -> start = Position(row, column)
             }
         }
     }
 
-    return sum
+    return Input(
+        start = start!!,
+        bounds = Bounds(lines.indices, lines.first().indices),
+        obstacles = obstacles
+    )
 }
 
-private fun part2(input: List<String>): Int = TODO()
+private typealias State = Pair<Position, Direction>
 
-private class Guard(val room: Matrix<Char>) {
-    var position: Position = room.find('^')
-    var direction: Direction = NORTH
+private data class Input(
+    val start: Position,
+    val obstacles: Set<Position>,
+    val bounds: Bounds
+)
 
-    fun walk() {
-        while (true) {
-            room[position] = 'X'
-            val nextPosition = position.nextInDirection(direction)
-            if (nextPosition.x !in room.columnIndices || nextPosition.y !in room.rowIndices) {
-                break
-            }
-            if (room[nextPosition] == '#') {
-                direction = direction.turnRight()
-                continue
-            }
-            position = nextPosition
-        }
+private data class Bounds(val rowBounds: IntRange, val columnBounds: IntRange) {
+    operator fun contains(position: Position): Boolean {
+        return position.x in rowBounds && position.y in columnBounds
     }
 }
