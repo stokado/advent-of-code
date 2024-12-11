ef0f3778d3b22e755a0a185c76eedb866bd6463a
Gleb Nazarov <gm.nazarov.work@gmail.com>
2024-12-11 11:56:59 +0500

    Day11: optimise solution, add part 2

--- advent-of-code-2024/src/Day11.kt
+++ advent-of-code-2024/src/Day11.kt
@@ -4,51 +4,54 @@ fun main() {
     val input = readInput(DAY)
     val testInput = readInput("${DAY}_test")
 
-    val part1Expected = 55312
+    val part1Expected = 55312L
     val part1Answer = part1(testInput)
 
     partResults("Part 1", part1Expected, part1Answer) { part1(input) }
 
-//    val part2Expected = 0
-//    val part2Answer = part2(testInput)
+    val part2Expected = 65_601_038_650_482L
+    val part2Answer = part2(testInput)
 
-//    partResults("Part 2", part2Expected, part2Answer) { part2(input) }
+    partResults("Part 2", part2Expected, part2Answer) { part2(input) }
 }
 
-private fun part1(input: List<String>): Int {
-    var newStones = input
-    for (i in 1..25) {
-        newStones = blink(newStones)
-    }
-    return newStones.size
-}
-
-private fun part2(input: List<String>): Int = TODO()
+private fun part1(input: List<Long>): Long = blink(input, 25)
+private fun part2(input: List<Long>): Long = blink(input, 75)
 
 
-private fun blink(stones: List<String>): List<String> {
-    return buildList {
-        for (stone in stones) {
-            val engravedNumber = stone.toLong()
+private fun blink(stones: List<Long>, totalBlinks: Int): Long {
+    data class State(val stone: Long, val step: Int)
 
-            if (engravedNumber == 0L) {
-                add("1")
-            } else if (stone.length % 2 == 0) {
-                val leftStone = stone.substring(0, stone.length / 2)
-                val rightStone = removeLeadingZeroes(stone.substring(stone.length / 2, stone.length))
-                add(leftStone)
-                add(rightStone)
-            } else {
-                val newValue = (engravedNumber * 2024).toString()
-                add(newValue)
-            }
+    val cache = mutableMapOf<State, Long>()
+    fun dfs(stone: Long, step: Int): Long {
+        if (step == totalBlinks) {
+            return 1L
         }
+
+        val state = State(stone, step)
+        if (state in cache) {
+            return cache[state]!!
+        }
+
+        val result = when {
+            stone == 0L -> dfs(1, step + 1)
+            stone.toString().length % 2 == 0 -> {
+                val string = stone.toString()
+                val middle = string.length / 2
+
+                val leftStone = string.substring(0..<middle).toLong()
+                val rightStone = string.substring(middle).toLong()
+
+                dfs(leftStone, step + 1) + dfs(rightStone, step + 1)
+            }
+            else -> dfs(stone * 2024, step + 1)
+        }
+        cache[state] = result
+        return result
     }
+
+    return stones.sumOf { stone -> dfs(stone, 0) }
 }
 
-private fun removeLeadingZeroes(str: String): String {
-    val trimmed = str.trimStart { it == '0' }
-    return trimmed.ifEmpty { "0" }
-}
 
-private fun readInput(name: String): List<String> = readText(name).split(" ")
+private fun readInput(name: String): List<Long> = readText(name).splitLongs()
