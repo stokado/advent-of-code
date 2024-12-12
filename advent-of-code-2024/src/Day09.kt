295485cdbfbf76240e19055634b27329800fcd71
Gleb Nazarov <gm.nazarov.work@gmail.com>
2024-12-13 00:23:03 +0500

    Day09: part 2 solution

--- advent-of-code-2024/src/Day09.kt
+++ advent-of-code-2024/src/Day09.kt
@@ -7,14 +7,15 @@ fun main() {
     fun testInput() = readInput("${DAY}_test")
 
     "Part 1" {
-        part1(testInput()) shouldBe 1928
+        part1(testInput()) shouldBe 1928L
         measureAnswer { part1(input()) }
     }
 
-//    "Part 2" {
-//        part2(testInput()) shouldBe 2858
-//        measureAnswer { part2(input()) }
-//    }
+    "Part 2" {
+        part2(testInput()) shouldBe 2858L
+        part2(input()) shouldBe 6287317016845L
+        measureAnswer { part2(input()) }
+    }
 }
 
 private fun part1(input: Pair<List<Int>, List<Int>>): Long {
@@ -55,7 +56,43 @@ private fun part1(input: Pair<List<Int>, List<Int>>): Long {
     return checkSum
 }
 
-private fun part2(input: List<String>): Int = TODO()
+private fun part2(input: Pair<List<Int>, List<Int>>): Long {
+    val (files, freeSpaces) = input
+
+    val filesIndexes = mutableMapOf<Int, Int>()
+    val freeSpaceRanges = mutableListOf<IntRange>()
+
+    var index = 0
+    for (cursor in files.indices) {
+        filesIndexes[cursor] = index
+        index += files[cursor]
+        if (cursor <= freeSpaces.lastIndex) {
+            freeSpaceRanges.add(index rangeOfSize freeSpaces[cursor])
+            index += freeSpaces[cursor]
+        }
+    }
+
+    var checksum = 0L
+
+    for (cursor in files.indices.reversed()) {
+        val fileSize = files[cursor]
+        val freeSpaceIndex = freeSpaceRanges.asSequence()
+            .take(cursor)
+            .indexOfFirst { it.size >= fileSize }
+
+        val fileStartIndex = if (freeSpaceIndex == -1) {
+            filesIndexes.getValue(cursor)
+        } else {
+            val range = freeSpaceRanges[freeSpaceIndex]
+            freeSpaceRanges[freeSpaceIndex] = range moveStartBy fileSize
+            range.start
+        }
+
+        checksum += checksumOf(fileStartIndex, fileId = cursor, fileSize = fileSize)
+    }
+
+    return checksum
+}
 
 private fun checksumOf(startIndex: Int, fileId: Int, fileSize: Int): Long {
     return (startIndex rangeOfSize fileSize).sumOf { index -> (fileId * index).toLong() }
--- advent-of-code-2024/src/libs/Ranges.kt
+++ advent-of-code-2024/src/libs/Ranges.kt
@@ -1,6 +1,10 @@
 package libs
 
+val ClosedRange<Int>.size: Int get() = endInclusive - start + 1
+
 infix fun Int.rangeOfSize(size: Int): IntRange {
     check(size >= 0) { "Size must be non-negative" }
     return this..<(this + size)
 }
+
+infix fun IntRange.moveStartBy(diff: Int) = (start + diff)..endInclusive
